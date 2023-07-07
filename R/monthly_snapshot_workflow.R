b544955ec4db3f3d497c7f8e84c2953b890c076f
hubert-crea <hubert@energyandcleanair.org>
2023-07-07 11:06:51 +0100

    -cleaning -new loaded functions

--- R/monthly_snapshot_workflow.R
+++ R/monthly_snapshot_workflow.R
@@ -1,4 +1,4 @@
-build_snapshot <- function(focus_month = NULL, output_dir = NULL) {
+build_snapshot <- function(focus_month = NULL, output_dir = NULL, lang="EN") {
   # required input data:
   # "data/monthly industry stats.xlsx"
   # "data/Power Capacity.xlsx"
@@ -27,13 +27,21 @@ build_snapshot <- function(focus_month = NULL, output_dir = NULL) {
   aq_dw <- get_deweathered_aq(china_admin_capitals, update_data = T)
 
   for (lang in c("EN", "ZH")) {
-    # focus_month <- ymd("2023-04-01")
+
     capacity_plots(focus_month = focus_month,
                    output_dir=output_dir,
-                   lang="EN")
-    # focus_month <- ymd("2023-05-01")
-    industry_output_plots(focus_month = focus_month)
-    fuel_supply_plots(focus_month = focus_month)
-    air_quality_plots(focus_month = focus_month, update_data = F, aq = aq, aq_dw = aq_dw)
+                   lang=lang)
+
+    industry_output_plots(focus_month = focus_month,
+                          output_dir=output_dir,
+                          lang=lang)
+
+    fuel_supply_plots(focus_month = focus_month,
+                      output_dir=output_dir,
+                      lang=lang)
+
+    air_quality_plots(focus_month = focus_month, update_data = F, aq = aq, aq_dw = aq_dw,
+                      output_dir=output_dir,
+                      lang=lang)
   }
 }
