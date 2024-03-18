b4a8bbf22b7bf407d4bbefa2cc2626b2e4c5b6f3
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2024-03-18 14:50:32 +0200

    workflows to plot province power generation and one month air quality trends

--- project_workflows/china capitals one month PM2.5 time series.R
+++ project_workflows/china capitals one month PM2.5 time series.R
@@ -1,27 +1,23 @@
+source('load_package.R')
+
+require(tidyverse)
+require(magrittr)
+require(lubridate)
+require(chinatracker)
+
+focus_month <- ymd('2023-12-01')
+
 aq_long <- get_aq(start_date=ymd('2017-01-01'),
                   update_data=T,
                   aq_file='cache/capitals_air_quality_data.RDS',
                   cities=china_admin_capitals)
 
-
-aq_long %>% filter(pollutant=='pm25') %>% add_location_names(country=country, lang=lang) -> aq_long_wnames
-
-aq_long_wnames %>% #rename(city_name=city_id) %>%
+aq_long %>% filter(pollutant=='pm25') %>%
+  add_location_names(country='CN') %>%
   group_by(city_name, month=date %>% 'day<-'(1), pollutant) %>%
   summarise(across(value, mean)) %>%
-  filter(pollutant=='pm25', month <= focus_month, month(month)==month(focus_month)) %>%
-  group_by(city_name) %>% mutate(rank=rank(value)) %>%
-  ggplot(aes(year(month), value, fill=rank)) + geom_col() + facet_wrap(~city_name) +
-  coord_cartesian(ylim=c(0,100)) +
-  labs(title='December average PM2.5 levels in province capitals', x='', y='ug/m3') +
-  theme_crea() + scale_fill_crea_c('change', guide='none', col.index = 5:7) +
-  x_at_zero()
+  filter(month(month)==month(focus_month)) ->
+  aq_focus_month
 
-
-
-aq_long_wnames %>% #rename(city_name=city_id) %>%
-  group_by(city_name, month=date %>% 'day<-'(1), pollutant) %>%
-  summarise(across(value, mean)) %>%
-  filter(pollutant=='pm25', month <= focus_month, month(month)==month(focus_month)) %>%
-  group_by(city_name) %>% filter(value[year(month)==2023]>max(value[year(month) %in% 2020:2022])) %>%
+aq_focus_month %>% group_by(city_name) %>% filter(value[year(month)==2023]>max(value[year(month) %in% 2020:2022])) %>%
   use_series(city_name) %>% unique %>% paste(collapse=', ')
--- project_workflows/province power generation trends.R
+++ project_workflows/province power generation trends.R
@@ -1,12 +1,14 @@
-output_dir="~/RPackages/gcpt-analysis/outputs/2023H1_analysis"
+source('load_package.R')
 
-in_file="data/power generation by province and source.xlsx"
+output_dir="~/RPackages/gcpt-analysis/outputs/2023H2_analysis"
+
+in_file=get_data_file("power generation by province and source.xlsx")
 getwindvars(in_file)
 readwindEN(in_file, c('var', 'source', 'prov'), read_vardata = T, columnExclude = 'Consumption', zero_as_NA = T) -> provgen
 readwindEN(in_file, c('var', 'prov'), read_vardata = T, columnFilter = 'Consumption', zero_as_NA = T) %>% mutate(source=var) -> provcons
 
 
-in_file="data/power capacity by province&type.xlsx"
+in_file=get_data_file("power capacity by province&type.xlsx")
 getwindvars(in_file)
 readwindEN(in_file, c('var', 'source', 'prov'), read_vardata = T, zero_as_NA = T) -> provcap
 
@@ -39,19 +41,23 @@ provpwr_norm %>% ungroup %>%
   ungroup %>% arrange(-thermal_share) %>% mutate(prov=factor(prov, unique(prov))) -> growth_mix_plot
 
 growth_mix_plot %>% filter(source=='Thermal') %>% data.frame
+lang='ZH'
+fuel_cols -> fuel_cols_plot
+names(fuel_cols_plot) %<>% trans
 
 growth_mix_plot %>%
-  ggplot(aes(prov, change, fill=source)) + geom_col(position='fill') + coord_flip() +
+  ggplot(aes(trans(prov), change, fill=trans(source))) + geom_col(position='fill') + coord_flip() +
   scale_y_continuous(expand=expansion(), labels=scales::percent) +
-  scale_fill_manual(values=fuel_cols, guide=guide_legend(nrow=1, reverse=T), name='') +
+  scale_fill_manual(values=fuel_cols_plot, guide=guide_legend(nrow=1, reverse=T), name='') +
   theme_crea(legend.position='top', plot.margin=unit(c(1.5, 2, .2, .2), 'line')) +
-  labs(title='Shares of power generation sources in growth from 2020 to present', x='', y='',
-       caption=paste('The present period refers to the most recent 12 months of data, up to', format(max(provpwr_norm$date), '%B %Y'))) ->
+  lang_theme() +
+  labs(title=trans('Shares of power generation sources in growth from 2020 to present'), x='', y='',
+       caption=paste(trans('The present period refers to the most recent 12 months of data, up to'), monthyearlab(max(provpwr_norm$date), english_format = '%B %Y'))) ->
   plt
 quicksave(file.path(output_dir,
                     paste0('Shares of power generation sources in growth from 2020 to ',
-                           format(max(provpwr_norm$date), '%B %Y'),'.png')), plot=plt, scale=1.1)
-
+                           format(max(provpwr_norm$date), '%B %Y'), '_', lang,'.png')), plot=plt, scale=1.1)
+growth_mix_plot %>% select(-thermal_share) %>% write_csv(file.path(output_dir, 'Shares of power generation sources in growth.csv'))
 
 provpwr_norm %>% ungroup %>%
   filter(date %in% c(ymd('2020-12-31'), max(date)), source=='Thermal') %>%
@@ -66,3 +72,40 @@ provpwr_norm %>% ungroup %>%
 quicksave(file.path(output_dir,
                     paste0('Changes in thermal power generation by province from 2020 to ',
                            format(max(provpwr_norm$date), '%B %Y'),'.png')), scale=1.1, plot=plt)
+
+
+fuel_cols_plot %<>% c(fuel_cols['Solar'], .)
+names(fuel_cols_plot)[1] <- trans('Wind&Solar')
+
+provpwr_norm %>% ungroup %>%
+  filter(date %in% max(date), !grepl('Cons', source)) %>%
+  mutate(source=ifelse(grepl('Wind|Solar', source), 'Wind&Solar', source)) %>%
+  group_by(prov, source) %>%
+  summarise(across(Value12m, sum)) %>%
+  group_by(prov) %>% mutate(share = Value12m/sum(Value12m, na.rm=T)) %>%
+  ungroup %>%
+  (function(df) {
+    df %>% filter(source=='Wind&Solar') -> df_thermal #Thermal
+    prov_order = df_thermal$prov[order(df_thermal$share)]
+    df %>% mutate(prov=factor(prov, prov_order))
+  }) %>%
+  mutate(source=factor(source, c('Thermal', 'Hydro', 'Nuclear', 'Solar', 'Wind', 'Wind&Solar')),
+         show_label=share>.15 | (grepl('Solar|Wind|Thermal', source) & share>.05)) %>%
+  write_csv(file.path(output_dir, 'Power generation mix by province.csv')) %>%
+  ggplot(aes(trans(prov), Value12m*12/1e5, fill=trans(source), group=source)) +
+  geom_col(position='fill') +
+  geom_text(aes(label=scales::percent(share, accuracy=1),
+                color=show_label, alpha=show_label),
+            position = position_fill(vjust = 0.5)) +
+  coord_flip() +
+  theme_crea() +
+  labs(title=trans('Power generation mix by province'), x='', y='',
+       caption=paste(trans('most recent 12 months of data, up to'), monthyearlab(max(provpwr_norm$date), english_format='%B %Y'))) +
+  scale_fill_manual(values=fuel_cols_plot,
+                    guide=guide_legend(ncol=1, reverse=T), name='') +
+  scale_y_continuous(expand=expansion(), labels=scales::percent) +
+  scale_color_manual(values=c(col.a('black', 0), col.a('white', 1)), guide='none') +
+  scale_alpha_manual(values=c(0,1), guide='none') -> plt
+quicksave(file.path(output_dir,
+                    paste0('power mix by province',
+                           format(max(provpwr_norm$date), '%B %Y'), "_", lang,'.png')), scale=1.1, plot=plt)
