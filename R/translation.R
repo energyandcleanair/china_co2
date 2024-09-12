b5257c9ecdea779eb321b40150b9483b529b3d4d
Danny Hartono <danny.hartono@outlook.com>
2024-09-12 18:20:41 +0700

    try fix ZH font

--- R/translation.R
+++ R/translation.R
@@ -115,7 +115,7 @@ unit_label <- function(original_unit, lang=get('lang', envir=.GlobalEnv)) {
 
 lang_theme <- function(lang=get('lang', envir=.GlobalEnv)) {
   if(lang == 'ZH'){
-    theme(text = element_text(face = 'bold', family = 'Noto Sans'))
+    theme(text = element_text(face = 'bold', family = 'wqy-microhei'))
   } else {
     theme()
   }
