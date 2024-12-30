73f5512ee57f26351d9c9185d44cdfe4088d100c
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2024-12-30 17:58:02 +0200

    update to work with 2024 data

--- project_workflows/dams_water_level.R
+++ project_workflows/dams_water_level.R
@@ -1,6 +1,13 @@
 
-readwindEN(get_data_file('China_Water_Level_Reservoirs.xlsx'),
-           c('var', 'V1', 'dam'), force_last_of_month = F, read_vardata = T, zero_as_NA = T) -> dams
+readwindEN(get_data_file('Hydropower_Reservoir_Water_Levels.xlsx'),
+           c('var', 'V1', 'dam'), force_last_of_month = F, read_vardata = T, zero_as_NA = T, skip=2) -> dams
+
+
+dams %<>%
+  mutate(large_dam=var=='Water Level',
+         dam=ifelse(!grepl('Water', var), var, dam)) %>%
+  filter(!grepl('Water Flow', var)) %>%
+  mutate(var='Water Level')
 
 mad_filter=10
 
@@ -18,35 +25,35 @@ dams %>% filter(!is.na(Value)) %>%
 dams_nomad %>% summary
 
 dams_nomad %>% mutate(var=var %>% tolower %>% gsub(' ', '_', .)) %>%
-  select(dam, date, var, Value) %>%
+  select(dam, large_dam, date, var, Value) %>%
   pivot_wider(names_from=var, values_from=Value) -> dams_wide
 
 dams_wide %<>%
   filter(year(date)>=2012) %>%
-  complete(dam, date) %>%
+  complete(nesting(dam, large_dam, var), date) %>%
   group_by(dam) %>%
   fill(water_storage_capacity, .direction='downup')
 
-dams_wide %<>%
+dams_wide %<>% group_by(dam) %>%
   mutate(water_level=na.approx(water_level, date, date, na.rm=F),
          water_level_deviation=water_level-mean(water_level, na.rm=T)) %>%
-  filter(!is.na(water_level)) %>% filter(2023 %in% year(date))
+  filter(!is.na(water_level)) %>% filter(all(2023:2024 %in% year(date)))
 
-dams_wide  %>% filter(date>='2017-01-07') %>%
+dams_wide  %>% filter(date>='2018-01-07') %>%
   mutate(plotdate=date %>% 'year<-'(2022), year=as.factor(year(date)),
          dam = dam %>% gsub(' \\(.*| Power| Reserv', '', .)) %>%
   ggplot(aes(plotdate, water_level_deviation)) +
   geom_line(aes(col=year, linewidth=year, alpha=year)) +
   facet_wrap(~dam) +
   theme_crea() +
-  labs(title='Water level in 13 hydropower reservoirs across China',
+  labs(title='Water level in 11 hydropower reservoirs across China',
        subtitle = 'Deviation from mean',
        y='meters', x='') +
   scale_x_date(date_labels = '%b') +
   scale_linewidth_discrete(range=c(.5,1.25)) +
   scale_alpha_discrete(range=c(.5,1)) +
-  scale_color_manual(values=colorspace::darken(crea_palettes$change, .2)) -> p
-quicksave('outputs/Water level in 16 hydropower reservoirs across China.png', plot=p)
+  scale_color_manual(values=unname(colorspace::darken(crea_palettes$change, .2))) -> p
+quicksave('outputs/latest/Water level in 11 hydropower reservoirs across China.png', plot=p)
 
 dams_wide %>% ungroup %>%
   filter(!grepl('14 pm', dam), date<max(date)-1) %>%
