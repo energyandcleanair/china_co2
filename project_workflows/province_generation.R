4a5132934bc01a530a8291d0f6b1cfd06a0aea73
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2024-12-31 10:16:28 +0200

    add analyses and graphs; separate data loading routines into a separate script

--- project_workflows/province_generation.R
+++ project_workflows/province_generation.R
@@ -1,143 +1,12 @@
 source('scripts/load_package.R')
+require(units)
 
 output_dir <- 'outputs/province_clean_power_analysis'
 
-getwindvars(get_data_file('wind_solar_utilization_capacity_by_province.xlsx'), skip=2)
+source('scripts/load_province_generation_data.R')
+save.image(file.path(output_dir, 'alldata.RData'))
+load(file.path(output_dir, 'alldata.RData'))
 
-readwindEN(get_data_file('power generation by province and source.xlsx'),
-           c('var', 'source', 'prov'), read_vardata = T,
-           columnExclude = 'Consumption') %>%
-  mutate(var='Generation') -> gen
-
-readwindEN(get_data_file('power generation by province and source.xlsx'),
-           c('var', 'prov'), read_vardata = T,
-           columnFilter = 'Consumption') -> cons
-
-readwindEN(get_data_file('wind_solar_utilization_capacity_by_province.xlsx'),
-           c('prov', 'var', 'source'),
-           columnExclude = 'National',
-           skip=2, read_vardata = T) -> cap_util
-
-readwindEN(get_data_file('power capacity by province&type.xlsx'),
-           c('prov', 'var', 'source'),
-           columnExclude = 'Wind|Solar',
-           skip=2, read_vardata = T) -> cap_all
-
-#fill nuclear forward
-cap_all %<>% mutate(Value=ifelse(Value==0, NA, Value)) %>% group_by(prov, source) %>% fill(Value, .direction='down')
-
-#combine
-bind_rows(gen, cons, cap_util, cap_all) %>%
-  mutate(source=disambiguate(source, c('Thermal', 'Wind', 'Solar', 'Hydro', 'Nuclear')),
-         var=disambiguate(var, c('Utilization', 'Capacity', 'Consumption'))) %>%
-  group_by(var, prov, source) %>%
-  mutate(Value=ifelse(Value==0, NA, Value)) %>%
-  filter(!grepl('6,000 Kilowatts', source)) ->
-  provdata
-
-#fill solar backward
-read_bp('G:/My Drive/CO2data/BPreviews/Statistical Review of World Energy Data.xlsx', sheet='(Solar|Wind) Capacity', startRow = 4, read_multiple = T) %>%
-  bind_rows() %>% filter(country=='China') -> ws_cap_natl
-
-ws_cap_natl %>% mutate(date=ymd(paste(year, 12, 31)),
-              source=disambiguate(variable, c('Solar', 'Wind'))) %>%
-  select(source, date, national_total_gw=value) %>%
-  left_join(provdata %>% filter(var=='Capacity', source %in% c('Solar', 'Wind')), .) %>%
-  group_by(prov, source) %>%
-  mutate(across(national_total_gw, ~na.approx(.x, na.rm=F)),
-         Value=na.cover(Value,
-                        Value[which(!is.na(Value))[1]] * national_total_gw / national_total_gw[which(!is.na(Value))[1]])) %>%
-  select(-national_total_gw) %>%
-  bind_rows(provdata %>% filter(!(var=='Capacity' & source %in% c('Solar', 'Wind')))) ->
-  provdata
-
-
-#calculate monthly values from YTD
-provdata %<>% filter(var %in% c('Generation', 'Utilization')) %>%
-  group_by(prov, source, var) %>% unYTD %>%
-  bind_rows(provdata %>% filter(var=='Capacity') %>% mutate(Value1m=Value))
-
-#eliminate nonsensical utilization values
-provdata %<>% mutate(Value1m=case_when(var=='Utilization' & Value1m>days_in_month(date)*24~NA,
-                                       Value1m<0~NA,
-                                       T~Value1m))
-
-#fill gaps in time series
-provdata %>% ungroup %>%
-  complete(prov, nesting(source, var), date) %>%
-  group_by(prov, source, var) %>%
-  mutate(Value1m=ifelse(Value1m<=0, NA, Value1m) %>% na.approx(na.rm=F)) ->
-  provdata_filled
-
-#add utilization when missing
-provdata_filled %<>% group_by(prov, source, date) %>%
-  filter('Utilization' %notin% var) %>%
-  summarise(Value1m=Value1m[var=='Generation']/Value1m[var=='Capacity']) %>%
-  mutate(var='Utilization') %>%
-  bind_rows(provdata_filled)
-
-#add a variant of generation keeping utilization at trend
-provdata_filled %<>%
-  filter(var=='Utilization') %>%
-  group_by(prov, source) %>%
-  filter(!all(is.na(Value1m))) %>%
-  group_modify(function(df, group) {
-    data_start=min(df$date[!is.na(df$Value1m)])
-    lm(Value1m~poly(pmax(ymd('2012-01-31'), data_start, date),2)+as.factor(month(date)),
-                    data=df %>% filter(year(date)>=2012)) -> m
-    df %>% mutate(Value1m=predict(m, .))
-  }) %>% bind_rows(provdata_filled %>% filter(var=='Capacity')) %>%
-  mutate(variant='Utilization at trend') %>%
-  bind_rows(provdata_filled %>% mutate(variant='Actual utilization'))
-
-provdata_filled %>% filter(variant=='Utilization at trend', var=='Utilization',
-                           source %in% c('Wind', 'Solar'), Value1m<30*24) %>%
-  ggplot(aes(date, Value1m, col=source)) + geom_line() + facet_wrap(~prov)
-
-#use trend utilization when actual not available
-provdata_filled %<>% filter(var=='Utilization') %>%
-  group_by(prov, date, source) %>%
-  fill(Value1m) %>%
-  bind_rows(provdata_filled %>% filter(var!='Utilization'))
-
-#calculate generation from capacity and utilization
-provdata_filled %<>% filter(var %in% c('Capacity', 'Utilization')) %>%
-  group_by(prov, source, date, variant) %>%
-  summarise(Value1m=Value1m[var=='Capacity']*Value1m[var=='Utilization']) %>%
-  mutate(var='Generation, calculated') %>%
-  bind_rows(provdata_filled)
-
-#adjust thermal generation in the trend utilization case
-provdata_filled %>%
-  filter(source != 'Thermal', var=='Generation, calculated') %>%
-  group_by(prov, date, source) %>%
-  summarise(deviation=Value1m[grepl('Actual', variant)]-Value1m[grepl('trend', variant)]) %>%
-  group_by(prov, date) %>%
-  summarise(across(deviation, ~sum(.x, na.rm=T))) ->
-  deviation
-
-provdata_filled %<>% left_join(deviation) %>%
-  group_by(prov, date) %>%
-  mutate(Value1m=case_when(source=='Thermal' & var=='Generation, calculated' & variant=='Utilization at trend' ~
-                             pmax(0, Value1m[source=='Thermal' & var=='Generation'] + deviation),
-                           T~Value1m))
-
-#fill trend generation when not available, using actual generation
-provdata_filled %<>% ungroup %>% filter(grepl('Generation', var)) %>%
-  complete(nesting(prov, source), variant, date, var) %>%
-  group_by(prov, date, source) %>%
-  fill(Value1m) %>%
-  bind_rows(provdata_filled %>% filter(!grepl('Generation', var)))
-
-
-provdata_filled %<>% group_by(var, prov, source, variant) %>% mutate(Value_rollmean_12m=zoo::rollapplyr(Value1m, 12, mean, fill=NA))
-
-
-
-provdata_filled %>%
-  filter(var=='Generation, calculated', year(date)>=2013) %>%
-  mutate(source=factor(source, levels=c('Thermal', 'Hydro', 'Nuclear', 'Wind', 'Solar'))) ->
-  plotdata
 
 fuel_cols <- crea_palettes$CREA[c('Light.gray', 'Blue', 'Dark.red', 'Dark.violet', 'Orange')]
 names(fuel_cols) <- c('Thermal', 'Hydro', 'Nuclear', 'Wind', 'Solar')
@@ -151,21 +20,6 @@ plotdata %>% filter(variant=='Utilization at trend') %>% #Utilization at trend |
   labs(title='Power generation mix by province', subtitle='12-month rolling mean', x='', y='') -> p
 quicksave(file.path(output_dir, 'Power generation mix by province.png'), plot=p)
 
-plotdata %>%
-  filter(variant=='Utilization at trend') %>% #Utilization at trend
-  replace_na(list(Value_rollmean_12m=0)) %>%
-  group_by(prov, date) %>%
-  mutate(share=Value_rollmean_12m/sum(Value_rollmean_12m)) ->
-  shares
-
-shares %>%
-  group_by(variant, prov, source) %>%
-  summarise(across(c(Value_rollmean_12m, share),
-                   ~.x[date==max(date)]-.x[date=='2020-12-31'])) ->
-  changes
-
-changes %>% filter(source!='Thermal') %>% group_by(prov) %>% summarise(across(is.numeric, sum)) %>%
-  mutate(source='Non-fossil total') -> changes_total
 
 changes %>% filter(source!='Thermal') %>%
   ggplot(aes(prov, share, fill=source)) +
@@ -255,42 +109,12 @@ quicksave(file.path(output_dir, 'Solar power capacity in China by province.png')
 
 
 
-#Analysis of factors
-readwindEN(get_data_file('Electricity Consumption by province_2023.xlsx'), c('prov', 'var'),
-           read_vardata = T, skip=3) -> power_demand
-
-readwindEN(get_data_file('Regional GDP complete.xlsx'), c('var', 'price_basis', 'sector', 'subsector'),
-           read_vardata = T, columnFilter = 'Constant|Current') -> gdp_natl
-
-readwindEN(get_data_file('Regional GDP complete.xlsx'), c('prov', 'var', 'sector', 'subsector'),
-           read_vardata = T, columnExclude = 'Constant|Current') %>%
-  mutate(subsector=disambiguate(subsector,
-                                c('Hotels and Catering Services'='Hotel',
-                                  Finance='Financ',
-                                  'Wholesale and Retail Trade'='Trade',
-                                  'Transport, Storage and Post'='Transport')),
-         across(contains('sector'), ~ifelse(.x=='YTD', NA, .x)),
-         type=ifelse(grepl('YTD', Name), 'YTD', 'single month')) -> gdp_prov
-
-readwindEN(get_data_file('iron&steel output by province.xlsx'),
-           c('prov', 'var', 'prod'),
-           read_vardata = T) %>% filter(!grepl('Taiwan', Name)) -> i_s
-
-readwindEN(get_data_file('Output of electricity intensive products by province.xlsx'),
-           c('prov', 'var', 'prod'),
-           read_vardata = T) -> ind
-
-ind %<>% bind_rows(i_s)
-
-read_xlsx('inst/extdata/population by province.xlsx') %>%
-  pivot_longer(-Region, names_to='year', values_to='pop') %>%
-  rename(prov=Region) -> pop
 
 #target dataset: increase in capacity, increase in generation, GDP growth, GDP per capita, industrial output & growth
 
 #analyses:
 #coal capacity additions vs coal power generation growth
-provdata_filled %>% group_by(prov) %>%
+provdata_filled %>% group_by(prov) %>% filter(!is.na(Value1m)) %>%
   filter(source=='Thermal', var %in% c('Capacity', 'Generation'), variant=='Utilization at trend') %>%
   mutate(value_to_plot = ifelse(var=='Capacity', Value1m, Value_rollmean_12m),
          Generation_volume=value_to_plot[var=='Generation' & date==max(date)]) %>%
@@ -300,26 +124,113 @@ provdata_filled %>% group_by(prov) %>%
 
 #wind and solar utilization vs additions
 provdata_filled %>% #filter(prov!='Tibet') %>%
+  filter(date<'2024-10-30') %>%
   group_by(prov, var, variant, date) %>%
   mutate(share=Value_rollmean_12m/sum(Value_rollmean_12m, na.rm=T),
          value_to_plot = ifelse(var %in% c('Capacity', 'Generation'), share, Value_rollmean_12m)) %>% #Value1m
   ungroup %>%
-  filter(source %in% c('Wind', 'Solar'), var %in% c('Capacity', 'Generation', 'Utilization'), date==max(date), variant=='Utilization at trend') %>%
+  filter(source %in% c('Wind', 'Solar'), var %in% c('Capacity', 'Generation', 'Utilization'),
+         date==max(date), variant=='Utilization at trend') %>%
   select(prov, source, var, value_to_plot) %>%
   spread(var, value_to_plot) %>%
   ggplot(aes(Utilization, Generation)) + geom_point() + facet_wrap(~source, scales='free') +
   geom_text_repel(aes(label=prov))
 
+
+provdata_filled %>% #filter(prov!='Tibet') %>%
+  filter(date<'2024-10-30') %>%
+  group_by(prov, var, variant, date) %>%
+  mutate(share=Value_rollmean_12m/sum(Value_rollmean_12m, na.rm=T),
+         value_to_plot = ifelse(var %in% c('Capacity'), Value1m, Value_rollmean_12m)) %>% #Value1m
+  ungroup %>%
+  filter(source %in% c('Wind', 'Solar'), var %in% c('Capacity', 'Utilization'),
+         date==max(date), variant=='Actual utilization') %>%
+  select(prov, source, var, value_to_plot) %>%
+  spread(var, value_to_plot) %>%
+  ggplot(aes(Utilization, Capacity)) + geom_point() + facet_wrap(~source, scales='free') +
+  geom_text_repel(aes(label=prov))
+
+
 #wind and solar additions vs population density
+adm1 %>% mutate(area_km2 = as.numeric(st_area(.)/1e6),
+                prov=fix_province_names(NAME_1)) %>%
+  st_drop_geometry() %>%
+  select(prov, area_km2) %>%
+  left_join(pop %>% filter(year==2023)) %>%
+  mutate(pop_per_km2=pop*1e4/area_km2) -> pop_density
+
+
+
+provdata_filled %>% filter(var=='Capacity', source %in% c('Wind', 'Solar'), !is.na(Value1m)) %>%
+  filter(date==max(date)) %>% ungroup %>% distinct(prov, source, .keep_all = T) %>%
+  select(prov, source, Value1m) %>%
+  left_join(pop_density %>% select(-pop)) %>%
+  left_join(gdp_pop %>% filter(is.na(subsector)) %>%
+              select(prov, date, sector, pop, gdp_12m) %>%
+              mutate(sector=paste('GDP', sector) %>% make_names) %>%
+              spread(sector, gdp_12m)) %>%
+  mutate(across(starts_with('gdp'), list(percap=.x/pop))) -> gdp_pop
+
+gdp_pop %>% lm(Value1m~(pop_per_km2+area_km2+pop+gdp_per_cap_12m):source, data=.) %>% summary
+
+gdp_pop %>%
+  ggplot(aes(pop_per_km2, Value1m)) + geom_point() + geom_text_repel(aes(label=prov)) + facet_wrap(~source) +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_smooth(method='lm')
 
 #generation growth rate vs increase in share of clean
-provdata_filled %>% #filter(prov!='Tibet') %>%
+provdata_filled %>% filter(prov!='Tibet') %>%
+  filter(var=='Generation', variant=='Utilization at trend', !is.na(Value_rollmean_12m)) %>%
   group_by(prov, var, variant, date) %>%
-  summarise(across()) %>%
-  bind_rows() #add totals
+  summarise(across(starts_with('Value'), sum)) %>%
+  summarise(across(starts_with('Value'), ~.x[date==max(date)]/.x[date=='2020-12-31']-1)) %>%
+  left_join(changes_total %>% select(prov, change_in_clean_share=share)) %>%
+  ggplot(aes(Value_rollmean_12m, change_in_clean_share)) + geom_point() + geom_text_repel(aes(label=prov))
 
 
 #demand growth vs GDP, industrial output growth
-#demand gowrth vs starting level per capita
+power_demand %<>% group_by(prov) %>%
+  mutate(Value12m=zoo::rollapplyr(Value, 10, mean, fill=NA))
+
+power_demand %>% group_by(prov) %>% summarise(across(Value, sum)) %>%
+  ggplot(aes(date, Value12m)) + geom_line()
+
+base_date=ymd('2019-12-31')
+years = as.numeric(round((max(gdp_prov$date) - base_date)/365*4, 0)/4)
+earlier_base_date=ymd('2015-12-31')
+earlier_years=as.numeric(round((base_date - earlier_base_date)/365*4, 0)/4)
+
+gdp_prov %>%
+  left_join(power_demand %>% select(prov, date, power_demand_12m=Value12m)) ->
+  gdp_pwr_prov
+
+gdp_pwr_prov %>% filter(year(date)>=2019) %>%
+  summarise(across(c(gdp_12m, power_demand_12m), ~(.x[date==max(date)]/.x[date==base_date])^(1/years)-1)) %>%
+  filter(sector=='Total', is.na(subsector)) %>%
+  ggplot(aes(gdp_12m, power_demand_12m)) + geom_point() + geom_abline() + geom_text_repel(aes(label=prov))
+
+#demand growth vs economic structure
+gdp_pwr_prov %>% group_by(prov, date) %>%
+  mutate(gdp_share_12m = gdp_12m/if_null(gdp_12m[sector=='Total'])) %>%
+  group_by(prov, sector, subsector) %>%
+  summarise(across(c(gdp_12m, power_demand_12m),
+                   list(growth=~(.x[date==max(date)]/.x[date==base_date])^(1/years)-1,
+                        earlier_growth=~(.x[date==base_date]/.x[date==earlier_base_date])^(1/earlier_years)-1)),
+            across(gdp_share_12m, list(change=~.x[date==max(date)] - .x[date==base_date]))) %>%
+  mutate(power_demand_12m_change=power_demand_12m_growth-power_demand_12m_earlier_growth,
+         gdp_12m_change=gdp_12m_growth-gdp_12m_earlier_growth) %>%
+  filter(sector=='Tertiary Industry', is.na(subsector)) %>%
+  ggplot(aes(gdp_share_12m_change, power_demand_12m_change)) + geom_point() +
+  geom_text_repel(aes(label=prov))
 
 
+#demand growth vs starting level per capita
+
+
+
+
+gdp_natl %>% group_by(price_basis, sector, subsector) %>%
+  mutate(gdp_12m=zoo::rollapplyr(Value, 4, sum, fill=NA)) %>%
+  filter(price_basis=='Current Prices') %>% filter(sector!='Total', grepl(' Industry', sector)) %>%
+  ggplot(aes(date, gdp_12m, fill=sector)) + geom_area(position='fill')
---
+++ scripts/load_province_generation_data.R
@@ -0,0 +1,203 @@
+readwindEN(get_data_file('power generation by province and source.xlsx'),
+           c('var', 'source', 'prov'), read_vardata = T,
+           columnExclude = 'Consumption') %>%
+  mutate(var='Generation') -> gen
+
+readwindEN(get_data_file('power generation by province and source.xlsx'),
+           c('var', 'prov'), read_vardata = T,
+           columnFilter = 'Consumption') -> cons
+
+readwindEN(get_data_file('wind_solar_utilization_capacity_by_province.xlsx'),
+           c('prov', 'var', 'source'),
+           columnExclude = 'National',
+           skip=2, read_vardata = T) -> cap_util
+
+readwindEN(get_data_file('power capacity by province&type.xlsx'),
+           c('prov', 'var', 'source'),
+           columnExclude = 'Wind|Solar',
+           skip=2, read_vardata = T) -> cap_all
+
+#fill nuclear forward
+cap_all %<>% mutate(Value=ifelse(Value==0, NA, Value)) %>% group_by(prov, source) %>% fill(Value, .direction='down')
+
+#combine
+bind_rows(gen, cons, cap_util, cap_all) %>%
+  mutate(source=disambiguate(source, c('Thermal', 'Wind', 'Solar', 'Hydro', 'Nuclear')),
+         var=disambiguate(var, c('Utilization', 'Capacity', 'Consumption'))) %>%
+  group_by(var, prov, source) %>%
+  mutate(Value=ifelse(Value==0, NA, Value)) %>%
+  filter(!grepl('6,000 Kilowatts', source)) ->
+  provdata
+
+#fill solar backward
+read_bp('G:/My Drive/CO2data/BPreviews/Statistical Review of World Energy Data.xlsx', sheet='(Solar|Wind) Capacity', startRow = 4, read_multiple = T) %>%
+  bind_rows() %>% filter(country=='China') -> ws_cap_natl
+
+ws_cap_natl %>% mutate(date=ymd(paste(year, 12, 31)),
+                       source=disambiguate(variable, c('Solar', 'Wind'))) %>%
+  select(source, date, national_total_gw=value) %>%
+  left_join(provdata %>% filter(var=='Capacity', source %in% c('Solar', 'Wind')), .) %>%
+  group_by(prov, source) %>%
+  mutate(across(national_total_gw, ~na.approx(.x, na.rm=F)),
+         Value=na.cover(Value,
+                        Value[which(!is.na(Value))[1]] * national_total_gw / national_total_gw[which(!is.na(Value))[1]])) %>%
+  select(-national_total_gw) %>%
+  bind_rows(provdata %>% filter(!(var=='Capacity' & source %in% c('Solar', 'Wind')))) ->
+  provdata
+
+
+#calculate monthly values from YTD
+provdata %<>% filter(var %in% c('Generation', 'Utilization')) %>%
+  group_by(prov, source, var) %>% unYTD %>%
+  bind_rows(provdata %>% filter(var=='Capacity') %>% mutate(Value1m=Value))
+
+#eliminate nonsensical utilization values
+provdata %<>% mutate(Value1m=case_when(var=='Utilization' & Value1m>days_in_month(date)*24~NA,
+                                       Value1m<0~NA,
+                                       T~Value1m))
+
+#fill gaps in time series
+provdata %>% ungroup %>%
+  complete(prov, nesting(source, var), date) %>%
+  group_by(prov, source, var) %>%
+  mutate(Value1m=ifelse(Value1m<=0, NA, Value1m) %>% na.approx(na.rm=F)) ->
+  provdata_filled
+
+#add utilization when missing
+provdata_filled %<>% group_by(prov, source, date) %>%
+  filter('Utilization' %notin% var) %>%
+  summarise(Value1m=Value1m[var=='Generation']/Value1m[var=='Capacity']) %>%
+  mutate(var='Utilization') %>%
+  bind_rows(provdata_filled)
+
+#add a variant of generation keeping utilization at trend
+provdata_filled %<>%
+  filter(var=='Utilization') %>%
+  group_by(prov, source) %>%
+  filter(!all(is.na(Value1m))) %>%
+  group_modify(function(df, group) {
+    data_start=min(df$date[!is.na(df$Value1m)])
+    lm(Value1m~poly(pmax(ymd('2012-01-31'), data_start, date),2)+as.factor(month(date)),
+       data=df %>% filter(year(date)>=2012)) -> m
+    df %>% mutate(Value1m=predict(m, .))
+  }) %>% bind_rows(provdata_filled %>% filter(var=='Capacity')) %>%
+  mutate(variant='Utilization at trend') %>%
+  bind_rows(provdata_filled %>% mutate(variant='Actual utilization'))
+
+provdata_filled %>% filter(variant=='Utilization at trend', var=='Utilization',
+                           source %in% c('Wind', 'Solar'), Value1m<30*24) %>%
+  ggplot(aes(date, Value1m, col=source)) + geom_line() + facet_wrap(~prov)
+
+#use trend utilization when actual not available
+provdata_filled %<>% filter(var=='Utilization') %>%
+  group_by(prov, date, source) %>%
+  fill(Value1m) %>%
+  bind_rows(provdata_filled %>% filter(var!='Utilization'))
+
+#calculate generation from capacity and utilization
+provdata_filled %<>% filter(var %in% c('Capacity', 'Utilization')) %>%
+  group_by(prov, source, date, variant) %>%
+  summarise(Value1m=Value1m[var=='Capacity']*Value1m[var=='Utilization']) %>%
+  mutate(var='Generation, calculated') %>%
+  bind_rows(provdata_filled)
+
+#adjust thermal generation in the trend utilization case
+provdata_filled %>%
+  filter(source != 'Thermal', var=='Generation, calculated') %>%
+  group_by(prov, date, source) %>%
+  summarise(deviation=Value1m[grepl('Actual', variant)]-Value1m[grepl('trend', variant)]) %>%
+  group_by(prov, date) %>%
+  summarise(across(deviation, ~sum(.x, na.rm=T))) ->
+  deviation
+
+provdata_filled %<>% left_join(deviation) %>%
+  group_by(prov, date) %>%
+  mutate(Value1m=case_when(source=='Thermal' & var=='Generation, calculated' & variant=='Utilization at trend' ~
+                             pmax(0, Value1m[source=='Thermal' & var=='Generation'] + deviation),
+                           T~Value1m))
+
+#fill trend generation when not available, using actual generation
+provdata_filled %<>% ungroup %>% filter(grepl('Generation', var)) %>%
+  complete(nesting(prov, source), variant, date, var) %>%
+  group_by(prov, date, source) %>%
+  fill(Value1m) %>%
+  bind_rows(provdata_filled %>% filter(!grepl('Generation', var)))
+
+
+provdata_filled %<>% group_by(var, prov, source, variant) %>% mutate(Value_rollmean_12m=zoo::rollapplyr(Value1m, 12, mean, fill=NA))
+
+
+
+provdata_filled %>%
+  filter(var=='Generation, calculated', year(date)>=2013) %>%
+  mutate(source=factor(source, levels=c('Thermal', 'Hydro', 'Nuclear', 'Wind', 'Solar'))) ->
+  plotdata
+
+
+
+plotdata %>%
+  filter(variant=='Utilization at trend') %>% #Utilization at trend
+  replace_na(list(Value_rollmean_12m=0)) %>%
+  group_by(prov, date) %>%
+  mutate(share=Value_rollmean_12m/sum(Value_rollmean_12m)) ->
+  shares
+
+shares %>%
+  group_by(variant, prov, source) %>%
+  summarise(across(c(Value_rollmean_12m, share),
+                   ~.x[date==max(date)]-.x[date=='2020-12-31'])) ->
+  changes
+
+changes %>% filter(source!='Thermal') %>% group_by(prov) %>% summarise(across(is.numeric, sum)) %>%
+  mutate(source='Non-fossil total') -> changes_total
+
+
+
+
+
+#Analysis of factors
+readwindEN(get_data_file('Electricity Consumption by province_2023.xlsx'), c('prov', 'var'),
+           read_vardata = T, skip=3) -> power_demand
+
+readwindEN(get_data_file('Regional GDP complete.xlsx'), c('var', 'price_basis', 'sector', 'subsector'),
+           read_vardata = T, columnFilter = 'Constant|Current') %>%
+  replace_na(list(sector='Total')) -> gdp_natl
+
+readwindEN(get_data_file('Regional GDP complete.xlsx'), c('prov', 'var', 'sector', 'subsector'),
+           read_vardata = T, columnExclude = 'Constant|Current') %>%
+  mutate(subsector=disambiguate(subsector,
+                                c('Hotels and Catering Services'='Hotel',
+                                  Finance='Financ',
+                                  'Wholesale and Retail Trade'='Trade',
+                                  'Transport, Storage and Post'='Transport')),
+         across(contains('sector'), ~ifelse(.x=='YTD', NA, .x)),
+         type=ifelse(grepl('YTD', Name), 'YTD', 'single month')) %>%
+  replace_na(list(sector='Total')) -> gdp_prov
+
+readwindEN(get_data_file('iron&steel output by province.xlsx'),
+           c('prov', 'var', 'prod'),
+           read_vardata = T) %>% filter(!grepl('Taiwan', Name)) -> i_s
+
+readwindEN(get_data_file('Output of electricity intensive products by province.xlsx'),
+           c('prov', 'var', 'prod'),
+           read_vardata = T) -> ind
+
+ind %<>% bind_rows(i_s)
+
+read_xlsx('inst/extdata/population by province.xlsx') %>% distinct %>%
+  pivot_longer(-Region, names_to='year', values_to='pop') %>%
+  mutate(across(year, as.numeric)) %>%
+  rename(prov=Region) -> pop
+
+#add total GDP when missing
+gdp_prov %<>% group_by(prov) %>% filter('Total' %notin% sector, is.na(subsector)) %>%
+  group_by(prov, date) %>% summarise(across(Value, sum)) %>%
+  mutate(sector='Total') %>% bind_rows(gdp_prov) %>% ungroup
+
+#gdp per capita
+gdp_prov %<>%
+  group_by(prov, sector, subsector) %>%
+  mutate(gdp_12m=zoo::rollapplyr(Value, 4, sum, fill=NA),
+         year=year(date)) %>%
+  left_join(pop) %>%
+  group_by(prov) %>% fill(pop, .direction = 'updown')
